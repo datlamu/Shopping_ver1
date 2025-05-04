ad3ca1f4986efb44a44a9eacae9f9604306c7279
datlamu <datthy26@gmail.com>
2025-05-04 17:14:02 +0700

    feat: View Component sidebar

---
+++ Shopping_ver1/Repository/Components/CategoriesViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shopping_ver1.Repository.Components
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        // Gọi db chỉ đọc và dùng trong class này
+        private readonly DataContext _dataContext;
+        public CategoriesViewComponent(DataContext context)
+        {
+            _dataContext = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _dataContext.Categories.ToListAsync());
+    }
+}
