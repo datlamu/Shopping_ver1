4b7b37e88c37d7a257b5b2f88ef831b23039c486
datlamu <datthy26@gmail.com>
2025-05-07 09:22:28 +0700

    feat: product-details

--- Shopping_ver1/Controllers/ProductController.cs
+++ Shopping_ver1/Controllers/ProductController.cs
@@ -1,17 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping_ver1.Repository;
 
 namespace Shopping_ver1.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly DataContext _dataContext;
+        public ProductController(DataContext context)
+        {
+            _dataContext = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Details()
+        public async Task<IActionResult> Details(int? id)
         {
-            return View();
+            if (id == null) return RedirectToAction("Index");
+            var productById = await _dataContext.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+            return View(productById);
         }
     }
 }
