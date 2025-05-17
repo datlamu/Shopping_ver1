c073ed896df120de7a4dddba0bf2c9deb7595585
datlamu <datthy26@gmail.com>
2025-05-17 17:23:44 +0700

    feat: image-preview-and-alert

---
+++ Shopping_ver1/wwwroot/backend/js/site.js
@@ -0,0 +1,22 @@
+﻿// Cảnh báo khi xóa và tự động ẩn thông báo
+$(function () {
+    $("a.confirmDeletion").on("click", function () {
+        return confirm("Confirm deletion");
+    });
+
+    setTimeout(() => {
+        $("div.alert.notification").fadeOut("slow");
+    }, 2000);
+});
+
+// Xem trước ảnh ở create product
+function previewImage(input, previewId = 'preview-img') {
+    const preview = document.getElementById(previewId);
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            preview.src = e.target.result;
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
