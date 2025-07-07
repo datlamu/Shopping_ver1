13748a166d167455fc04be706d7eb3c6fdf9e806
datlamu <datthy26@gmail.com>
2025-07-07 12:30:33 +0700

    feat: create FormatCurrency Helper, update all product price

--- Shopping_ver1/Controllers/ProductController.cs
+++ Shopping_ver1/Controllers/ProductController.cs
@@ -11,11 +11,41 @@ namespace Shopping_ver1.Controllers
         {
             _dataContext = context;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var product = await _dataContext.Products.ToListAsync();
+            return View(product);
         }
 
+        // Tìm kiếm sản phẩm
+        [HttpGet]
+        public async Task<IActionResult> Search(string? searchItem)
+        {
+            if (string.IsNullOrWhiteSpace(searchItem))
+                return RedirectToAction("Index");
+
+            var lowerSearch = searchItem.ToLower();
+
+            var products = await _dataContext.Products
+                .Include(p => p.Brand)
+                .Include(p => p.Category)
+                .Where(p =>
+                    p.Name.ToLower().Contains(lowerSearch) ||
+                    p.Description.ToLower().Contains(lowerSearch) ||
+                    p.Brand.Name.ToLower().Contains(lowerSearch) ||
+                    p.Brand.Description.ToLower().Contains(lowerSearch) ||
+                    p.Category.Name.ToLower().Contains(lowerSearch) ||
+                    p.Category.Description.ToLower().Contains(lowerSearch)
+                )
+                .ToListAsync();
+
+            ViewData["Keyword"] = searchItem;
+
+            return View("Index", products);
+        }
+
+        // Chi tiết sản phẩm
+        [HttpPost]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null) return RedirectToAction("Index");
---
+++ Shopping_ver1/Helpers/FormatCurrency.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopping_ver1.Helpers
+{
+    public static class FormatCurrency
+    {
+        public static string ToVnCurrency(decimal price)
+        {
+            return string.Format("{0:#,0}", (int)price).Replace(",", ".") + " VNĐ";
+        }
+    }
+}
